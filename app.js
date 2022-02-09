79ed08a0637341c7c5398b761c9ee0068cdab6a3
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-02-09 16:22:39 -0600

    added boxes.

---
+++ app.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let button = document.createElement('button');
+    button.innerText = 'Add Square';
+    document.body.appendChild(button);
+    divCounter = 1;
+    let colors = [
+        'red',
+        'green',
+        'blue',
+        'cyan',
+        'magenta',
+        'yellow',
+        'black',
+        'purple'
+    ];
+    button.addEventListener('click', function () {
+        let div = document.createElement('div');
+        document.body.appendChild(div);
+        div.id = divCounter;
+        div.className = 'squareOne';
+        divCounter++;
+
+        div.addEventListener("mouseover", function () {
+            div.innerText = div.id;
+            div.addEventListener("mouseout", function () {
+                div.innerText = "";
+            })
+            div.addEventListener("click", function () {
+                div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+            })
+            div.addEventListener("dblclick", idCheck);
+
+        })
+        function idCheck() {
+
+            if (div.id % 2 === 0) {
+                if (div.nextSibling === null) {
+                    alert("No box!!!!");
+                } div.nextSibling.remove();
+
+            }
+            else if (div.id % 2 !== 0) {
+                if (div.previousSibling === button) {
+                    alert("No box!!!!");
+                } div.previousSibling.remove();
+
+            }
+        }
+    })
+
+});
+
+
